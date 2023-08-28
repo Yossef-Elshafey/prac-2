9bb708328ade410850dda0fe6492ec75a47ef925
Yossef Elshafey <yssef0472@gmail.com>
2023-08-28 09:49:45 +0300

    delay file

---
+++ app/component/Delay.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import React from "react";
+
+function Delay() {
+  return (
+    <div className="absolute left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 ">
+      <Image
+        src="/logo.png"
+        alt="lg"
+        width={200}
+        height={200}
+        priority
+        className="animate-loader"
+      />
+    </div>
+  );
+}
+
+export default Delay;
